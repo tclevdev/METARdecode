9164c3a1508f2cbade447009b66903cd24de42b3
tclevdev <91774598+tclevdev@users.noreply.github.com>
2021-10-02 13:40:14 -0500

    Now will retrieve METAR from AVWX API with ICAO Code

--- METARdecode/METARdecode/Program.cs
+++ METARdecode/METARdecode/Program.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace METARdecode
 {
     class Program
     {
+        static string GetMetar(string code)
+        {
+            string responseString = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("Authorization", "Token yrUG0HlkEFgXd6Hjo_gRbOC4Hb6U8oZlR4L1Fr4H4tw");
+                var response = client.GetAsync($"https://avwx.rest/api/metar/{code}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+                    string rData = responseContent.ReadAsStringAsync().Result;
+                    JObject jsonData = JObject.Parse(rData);
+                    responseString = jsonData.SelectToken("raw").ToString();
+                }
+            }
+
+            return responseString;
+        }
+
         static void Main(string[] args)
         {
             Decoder decoder = new Decoder();
-            METAR metar = decoder.ProcessMetar("KAUS 021553Z 09004KT 10SM SCT013 BKN027 OVC250 24/22 A3008 RMK AO2 SLP178 T02440217");
+            METAR metar = new METAR();
+
+            //if (args[0].Length == 4)
+            //{
+                // Just an ICAO Code, look up the METAR via AVWX API
+                string rawMetar = GetMetar("KSFO");
+                metar = decoder.ProcessMetar(rawMetar);
+            //}
+            //else
+            //{
+                // Use command line argument
+            //    metar = decoder.ProcessMetar(args[0]);
+            //}
+
             string report = decoder.BuildReport(metar);
 
             Console.WriteLine(report);
